a5fbe4dc4ba0a8330fa73df89850b77d79f09ff7
kwonih1020 <kwonih1020@gmail.com>
2023-03-06 19:06:44 +0900

    added description comments

--- src/global_elements/NewGlobalModal.jsx
+++ src/global_elements/NewGlobalModal.jsx
@@ -11,7 +11,9 @@ const NewGlobalModal = ({ modalOption }) => {
                     <Contents>
                         <Content>
                             <h2>{modalOption?.title}</h2>
-                            {modalOption?.element}
+                            {modalOption?.element}\
+                            {/* 모달 컴포넌트 스타일링은 이렇게 준비했습니다. 모달마다 폼, 이벤트 문구, 
+                            사진 등등 원하는 element구조가 있을 수 있기 때문에 h2 태그 밑에 자리를 마련해놨습니다.  */}
                         </Content>
 
                         <ButtonBox>
--- src/hooks/useModal.js
+++ src/hooks/useModal.js
@@ -1,14 +1,16 @@
 import { useState, useCallback } from "react";
 
 const OPTION = {
-    show: false,
-    title: "",
-    onSubmit: () => {},
-    onClose: () => {},
-    element: null,
+    show: false, // 모달을 키고 끄는 옵션
+    title: "", // 모달의 문구
+    onSubmit: () => {}, // 모달을 킬 때마다 사용할 콜백 함수
+    onClose: () => {}, // 모달을 끌 때마다 사용할 콜백 함수
+    element: null, // 모달마다 넣고 싶은 추가 컴포넌트 자리
 };
 
 const useModal = () => {
+    // 모달마다 확인, 닫기 버튼을 누를 때마다 API 요청이나 원하는 로직을 같이 실행해주기를
+    // 원할 수 있기 때문에 사용하는 곳마다 콜백함수를 인자로 넣어주면 되겠습니다.
     const [modalOption, setModalOption] = useState(OPTION);
 
     const showModal = useCallback(
